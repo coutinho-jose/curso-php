08b62333b88be2039848ae5ba5c27cad3a65235f
Coutinho José Minêz <coutinhojoseminez09@gmail.com>
2026-09-05 19:11:31 +0200

    Atualiza aula 6

--- aula6/calculadora.php
+++ aula6/calculadora.php
@@ -9,9 +9,7 @@
 <body>
 
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-
-        <input type="number" step="any" name="num01" id="num01" placeholder="Primeiro Número" required>
-
+        <input type="number" name="num01" id="num01" placeholder="Primeiro Número" required>
         <select name="operador" required>
             <option value="">Oper</option>
             <option value="add">+</option>
@@ -19,32 +17,33 @@
             <option value="mult">*</option>
             <option value="div">/</option>
         </select>
-
-        <input type="number" step="any" name="num02" id="num02" placeholder="Segundo Número" required>
-
+        <input type="number" name="num02" id="num02" placeholder="Segundo Número" required>
         <button type="submit">Calcular</button>
+    </form>
 
         <?php 
+        // Pegando dados.
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $num01 = filter_input(INPUT_POST, "num01", FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-                $num02 = filter_input(INPUT_POST, "num02", FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+                $num01 = filter_input(INPUT_POST, "num01", FILTER_SANITIZE_NUMBER_FLOAT);
+                $num02 = filter_input(INPUT_POST, "num02", FILTER_SANITIZE_NUMBER_FLOAT);
                 $operador = htmlspecialchars($_POST["operador"]);
 
+                // Tratando erros.
                 $_erro = false;
 
-                if (empty($num01) && $num01 !== "0" || empty($num02) && $num02 !== "0" || empty($operador)) {
-                    echo "<p>Preencha todos os campos e selecione uma operação válida.</p>";
+                if (empty($num01) || empty($num02) || empty($operador)) {
+                    echo "<p class='erro-calc'>Preencha todos os campos.</p>";
                     $_erro = true;
                 }
 
                 if (!is_numeric($num01) || !is_numeric($num02)) {
-                    echo "<p>Preencha todos os campos apenas com números.</p>";
+                    echo "<p class='erro-calc'>Preencha todos os campos apenas com números.</p>";
                     $_erro = true;
                 }
 
+                // Calcular os números se não tiver erros.
                 if (!$_erro) {
                     $valor = 0;
-                    $exibirResultado = true;
 
                     switch ($operador) {
                         case "add":
@@ -60,26 +59,17 @@
                             break;
 
                         case "div":
-                            if ($num02 == 0) {
-                                echo "<p>Erro: Divisão por zero não é permitida.</p>";
-                                $exibirResultado = false;
-                            } else {
                                 $valor = $num01 / $num02;
-                            }
                             break;
 
                         default:
-                            echo "<p>Operação inválida.</p>";
-                            $exibirResultado = false;
+                            echo "<p class='erro-calc'>Operação inválida.</p>";
+                            break;
                     }
-
-                    if ($exibirResultado) {
-                        echo "<p>Resultado: " . $valor . "</p>";
-                    }
+                    echo "<p class='result-calc'>Resultado = " . $valor .  "</p>";
                 }
             }
         ?>
-    </form>
 
 </body>
 </html>
