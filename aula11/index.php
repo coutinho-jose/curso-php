a3c4a16fd5548f225d0effaf3e7d7583a6c12f6a
Coutinho José Minêz <coutinhojoseminez09@gmail.com>
2026-09-05 22:38:00 +0200

    Adiciona aula 11

---
+++ aula11/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 11 - Constantes em PHP</title>
+</head>
+<body>
+    
+
+    <?php 
+    
+        $nome = "Coutinho";
+        $nome = "José";
+        echo $nome . "<br>";
+
+        define("PI", 3.14);
+        define("NOME", "Coutinho");
+        define("IS_ADMIN", true);
+        echo PI;
+    
+    ?>
+
+</body>
+</html>
