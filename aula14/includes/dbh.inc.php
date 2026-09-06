0bf3e50c92ddd3f7c3780817880579476c2e0977
Coutinho José Minêz <coutinhojoseminez09@gmail.com>
2026-09-06 21:45:54 +0200

    Adiciona aula 14

---
+++ aula14/includes/dbh.inc.php
@@ -0,0 +1,15 @@
+<?php 
+
+    $dsn = "mysql:host=localhost;dbname=primeirobd";
+    $dbuser = "root";
+    $dbpass = "";
+
+    try{
+        $pdo = new PDO($dsn, $dbuser, $dbpass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    }catch(PDOException $e){
+        echo "Falha na conexão: " . $e->getMessage();
+    }
+
+    
--- aula14/index.php
+++ aula14/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula 13 - Base de Dados(MySQL) em PHP</title>
+    <title>Aula 14 - Base de Dados(MySQL) em PHP</title>
 </head>
 <body>
     
